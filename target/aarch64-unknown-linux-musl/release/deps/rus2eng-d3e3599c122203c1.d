0479b037e4350e18f74b1ea40f6499851eb0eeda
Igor Sokolov <isklv@users.noreply.github.com>
2026-07-27 11:55:37 +0000

    static musl binary, no dynamic deps

---
+++ target/aarch64-unknown-linux-musl/release/deps/rus2eng-d3e3599c122203c1.d
@@ -0,0 +1,5 @@
+/opt/data/rus2eng/target/aarch64-unknown-linux-musl/release/deps/rus2eng-d3e3599c122203c1.d: src/main.rs
+
+/opt/data/rus2eng/target/aarch64-unknown-linux-musl/release/deps/rus2eng-d3e3599c122203c1: src/main.rs
+
+src/main.rs:
